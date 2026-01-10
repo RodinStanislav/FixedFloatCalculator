a7a37b64a6bd7ab024758e4c4b35e4a3a6f68611
Станислав Родин <s.rodin@innopolis.ru>
2026-01-11 01:45:08 +0300

    fix linux build

--- main.cpp
+++ main.cpp
@@ -5,26 +5,25 @@
 #include "CalculatorBackend.h"
 #include "CalculatorView/CalculatorInputValidator.h"
 
+int main(int argc, char *argv[]) {
+  QGuiApplication app(argc, argv);
+  QQmlApplicationEngine engine;
 
-int main(int argc, char *argv[])
-{
-    QGuiApplication app(argc, argv);
-    QQmlApplicationEngine engine;
+  CalculatorBackend calculatorBackend;
 
-    CalculatorBackend calculatorBackend;
+  // the QML_ELEMENT way is currently not working
+  // (https://bugreports.qt.io/browse/QTBUG-93443)
+  qmlRegisterType<CalculatorInputValidator>("CalculatorInputValidator", 1, 0,
+                                            "CalculatorInputValidator");
 
-    // the QML_ELEMENT way is currently not working (https://bugreports.qt.io/browse/QTBUG-93443)
-    qmlRegisterType<CalculatorInputValidator>("CalculatorInputValidator", 1, 0, "CalculatorInputValidator");
+  QObject::connect(
+      &engine, &QQmlApplicationEngine::objectCreationFailed, &app,
+      []() { QCoreApplication::exit(-1); }, Qt::QueuedConnection);
 
-    QObject::connect(
-        &engine,
-        &QQmlApplicationEngine::objectCreationFailed,
-        &app,
-        []() { QCoreApplication::exit(-1); },
-        Qt::QueuedConnection);
+  engine.rootContext()->setContextProperty("calculatorBackend",
+                                           &calculatorBackend);
 
-    engine.loadFromModule("Calculator", "Main");
-    engine.rootContext()->setContextProperty("calculatorBackend", &calculatorBackend);
+  engine.loadFromModule("Calculator", "Main");
 
-    return app.exec();
+  return app.exec();
 }
